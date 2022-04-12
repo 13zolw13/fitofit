0de8712a224e39ca88245c55427b30c3ee771f7c
tomek <tomaszpawelkowalski@gmail.com>
2022-04-12 21:19:29 +0200

    Feat(back) set up mongo for user.

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private user: Model<UserDocument>) {}
+
+  async create(user: User): Promise<UserDocument> {
+    return await this.user.create(user);
+  }
+}
