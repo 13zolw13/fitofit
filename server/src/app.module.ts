ee689e02e6e8dd04c41cd41c115ddd586e01e189
tomek <tomaszpawelkowalski@gmail.com>
2022-04-12 10:29:48 +0200

    creat workout service.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkoutService } from './workout/service/workout/workout.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WorkoutService],
 })
 export class AppModule {}
