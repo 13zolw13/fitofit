3675d107346a3abebb0338221539d9ec205a9ad3
tomek <tomaszpawelkowalski@gmail.com>
2022-04-14 23:00:28 +0200

    Bag(back) migration.

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -2,8 +2,8 @@ export default {
   type: 'postgres',
   schema: 'public',
   url: 'postgres://postgres:postgres@127.0.0.1:5432/workout_test',
-  entities: ['./src/**/*.entity.ts'],
-  migrations: ['./src/migrations/*.ts'],
+  entities: ['src/**/*.entity{ .ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
   },
